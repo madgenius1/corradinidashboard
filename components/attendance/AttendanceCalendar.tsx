22547a756206b5fa426de3b328944d680071efec
ME <nickjuma@outlook.com>
2025-12-18 10:51:04 +0300

    Modified temporarily and note, should be undone: app/dashboard/admin/fees/structure/page.tsx         modified:   components/attendance/AttendanceCalendar.tsx         modified:   components/attendance/AttendanceGrid.tsx         modified:   components/charts/StatsChart.tsx         modified:   components/classes/ClassCard.tsx         modified:   components/classes/ClassForm.tsx         modified:   components/exports/ExportApprovalCard.tsx         modified:   components/exports/ExportRequestForm.tsx         modified:   components/grades/GradeChart.tsx         modified:   components/grades/GradeTable.tsx         modified:   components/layout/AdminSidebar.tsx         modified:   components/layout/RoleGuard.tsx         modified:   components/layout/TeacherSidebar.tsx         modified:   components/payments/FeeStructureTable.tsx         modified:   components/payments/PaymentForm.tsx         modified:   components/payments/PaymentTable.tsx         modified:   components/students/StudentCard.tsx         modified:   components/students/StudentForm.tsx         modified:   components/students/StudentTable.tsx         modified:   components/teachers/TeacherCard.tsx         modified:   components/teachers/TeacherForm.tsx

---
+++ components/attendance/AttendanceCalendar.tsx
@@ -0,0 +1,11 @@
+
+export default function Page() {
+    return(
+        <main>
+            <div>
+                <h2 className="text-xl text-gray-900">Item to be changed</h2>
+            </div>
+        </main>
+
+    )
+}
